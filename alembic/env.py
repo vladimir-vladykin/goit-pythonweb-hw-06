429879b844e6d7d35238e886af80439cb8c2ad8c
Volodymyr Vladykin <vova@pocketguard.com>
2025-10-01 15:04:06 +0300

    Fix setup script for alembic

--- alembic/env.py
+++ alembic/env.py
@@ -58,6 +58,7 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
+    config.set_main_option("sqlalchemy.url", SQLALCHEMY_DATABASE_URL)
     connectable = engine_from_config(
         config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
--- src/connect.py
+++ src/connect.py
@@ -1,9 +1,9 @@
 from sqlalchemy.engine import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# docker run --name orm-postgres-container -p 5432:5432 -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=567234 -d postgres
+# docker run --name orm-postgres-container -p 5432:5432 -e POSTGRES_DB=orm-university -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=567234 -d postgres
 SQLALCHEMY_DATABASE_URL = (
-    f"postgresql+psycopg2://postgres:567234@postgres:5432/orm-university"
+    f"postgresql+psycopg2://postgres:567234@localhost:5432/orm-university"
 )
 
 
